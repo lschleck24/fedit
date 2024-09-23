6ef0f76ae865b7132cb9c3c07745b1fedcb9b690
lschleck24 <luke.schleck@gmail.com>
2024-09-23 12:08:10 -0400

    Initial Commit

---
+++ fedit.c
@@ -0,0 +1,16 @@
+#include "mu.h"
+
+int 
+main(int argc, char *argv[])
+{
+    /* 
+     * TODO: delete the two MU_UNUSED lines below (they are just there so that
+     * this file compiles without warnings) and implement the project.  You'll
+     * want to parse the command-line arguments in main and create other
+     * functions as needed.
+     */
+    MU_UNUSED(argc);
+    MU_UNUSED(argv);
+    
+    return 0;
+}
