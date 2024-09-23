7b9b4ed9a07df12f94ca2c5dc154e75adb5c19ee
lschleck24 <luke.schleck@gmail.com>
2024-09-23 18:08:11 -0400

    Have switch statement. Handling most of the incorrect options for input

--- fedit.c
+++ fedit.c
@@ -3,6 +3,50 @@
 #include "stdio.h"
 #include "getopt.h" 
 #include <string.h>
+#include <stdbool.h>
+void rotate_left(FILE *file, int rotation_bytes) {
+    // Determine the size of the file
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    if (file_size == 0) return; // If the file is empty, do nothing
+
+    // Normalize the rotation bytes
+    //rotation_bytes = rotation_bytes % file_size;
+
+    // Create a buffer to hold the file's contents
+    unsigned char *buffer = malloc(file_size);
+    if (!buffer) {
+        perror("Failed to allocate memory");
+        exit(1);
+    }
+
+    // Read the entire file into the buffer
+    fread(buffer, 1, file_size, file);
+
+    // Create a new buffer for the rotated contents
+    unsigned char *rotated = malloc(file_size);
+    if (!rotated) {
+        perror("Failed to allocate memory");
+        free(buffer);
+        exit(1);
+    }
+
+    // Perform the rotation
+    for (long i = 0; i < file_size; i++) {
+        long new_pos = (i + file_size - rotation_bytes) % file_size; // Calculate new position
+        rotated[i] = buffer[new_pos]; // Assign the byte to the new position
+    }
+
+    // Write the rotated contents back to the file
+    fseek(file, 0, SEEK_SET); // Go back to the start of the file
+    fwrite(rotated, 1, file_size, file); // Write the rotated bytes
+
+    // Clean up
+    free(buffer);
+    free(rotated);
+}
 
 int 
 main(int argc, char *argv[])
@@ -16,6 +60,8 @@ main(int argc, char *argv[])
     int opt;
     int contract_flag = 0;
     int expand_flag = 0;
+    char *filename = NULL;
+    //int rotate_amount;
 
     const char *short_opts = "hl:x:c:v:k:s:";
     struct option long_opts[] = {
@@ -29,17 +75,24 @@ main(int argc, char *argv[])
         {NULL, 0, NULL, 0}
     };
 
-
+    filename = argv[argc-1];    
+    optind = 1;
     while((opt = getopt_long(argc, argv, short_opts,long_opts,NULL))!=-1){
+        if (opt=='h'){
+            printf("usage : \nfedit [-h] [-l NROTL] [-r NROTR] [-x NEXPAND]"
+            "[-c NCONTRACT] [-v CHAR] [-s NSKIP] [-k NKEEP] [-r] PATH\n");
+            return 0;
+        }
+        FILE *file = fopen(filename, "r+");
+        if (!file) {
+            perror("Error opening file");
+            exit(2);
+        }
         switch (opt){
-            case 'h':
-                printf("usage : \nfedit [-h] [-l NROTL] [-r NROTR] [-x NEXPAND]"
-                "[-c NCONTRACT] [-v CHAR] [-s NSKIP] [-k NKEEP] [-r] PATH\n");
-                break;
             case 'c':{
                 if(expand_flag==1){
-                    printf("not allowed to contract and expand at same time");
-                    return 1;
+                    fprintf(stderr,"not allowed to contract and expand at same time\n");
+                    exit(1);
                 }
                 printf("Hello World\n");
                 contract_flag=1;
@@ -47,16 +100,42 @@ main(int argc, char *argv[])
             }
             case 'x':{
                 if(contract_flag==1){
-                    printf("We are getting HERE GUYS");
-                    fprintf(stderr,"not allowed to contract and expand at the same time");
-                    return 1;
+                    printf("We are getting HERE GUYS\n");
+                    fprintf(stderr,"not allowed to contract and expand at the same time\n");
+                    exit(1);
                 }
                 printf("Hello SIr\n");
                 expand_flag=1;
                 break;
             }
+            case 'k':{
+                if(optind < argc && argv[optind][0] != '-'){
+                    fprintf(stderr,"Missing argument\n");
+                    exit(1);
+                }
+                break;
+            }
+            case 'l':{
+                printf("SIRRRRR\n");
+                int rotation_size = atoi(optarg);
+                printf("%d\n",rotation_size);
+                if(rotation_size<0){
+                    fprintf(stderr,"RAAA\n");
+                    exit(1);
+                }
+                rotate_left(file,rotation_size);
+                break;
+            }
+            case '?':{
+                //fprintf(stderr,"Unknown Operation");
+                exit(1);
+                break;
+            }
+            default:{
+                exit(1);
+            }
+
         }
     }
-    
     return 0;
 }
